827afe2408602fd83517cca13d4bfb7bcfaa7f33
jangsumin <jsm0726@naver.com>
2022-10-01 20:24:37 +0900

    uploading rush02

---
+++ rush02/ex00/includes/ft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sumijang <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/01 15:30:50 by sumijang          #+#    #+#             */
+/*   Updated: 2022/05/01 16:36:32 by sumijang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+# include <unistd.h>
+# include <fcntl.h>
+
+void	ft_putstr(char *str);
+int		sad_pisciner(void);
+int		luffy(void);
+int		starfish(void);
+
+#endif
